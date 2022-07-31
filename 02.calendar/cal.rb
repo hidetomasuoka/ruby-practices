b90af97371b59f9f951edaedeb38a402d18bfad4
Hideto Masuoka <hidemasuoka112@gmail.com>
2022-07-31 11:24:50 +0900

    リファクタリング

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -41,42 +41,20 @@ year  = Date.today.year if year.nil?
 month = Date.today.month if month.nil?
 
 calendar_header = "#{month}月 #{year}"
-
-first_day = Date.new(year.to_i, month.to_i, 1).day
-last_day = Date.new(year.to_i, month.to_i, -1).day
-space=" "
-week=""
-(first_day..last_day).each do |day|
-  days_week = Date.new(year.to_i, month.to_i, day).cwday 
-
-  if day==first_day 
-    first_day_week = Date.new(year.to_i, month.to_i, 1).cwday 
-    if first_day_week ==7
-      week=[week,day].join(space*(first_day_week*2))
-    else
-      week=[week,day].join(space*(first_day_week*2+4))
-    end
-  end
-  if first_day < day && day < 10 && days_week != 7 
-    week=[week,day].join(space*2)
-  end
-  if first_day < day && day < 10 && days_week == 7 
-    week=[week,day].join(space)
-  end
-     
-  if first_day < day && day >= 10 && days_week != 7 
-    week=[week,day].join(space)
-  end
-  if  first_day < day && day >= 10 && days_week == 7 
-    week.concat day.to_s
-  end
-
-  if days_week == 6 
-    week.concat("\n")
-  end
-end
-
+firstday_wday = Date.new(year.to_i,month.to_i,1).wday
 
 puts calendar_header.center(20)
 puts ('Su Mo Tu We Th Fr Sa')
-puts week
+print "   " * firstday_wday 
+
+
+((Date.new(year.to_i, month.to_i, 1).day)..(Date.new(year.to_i, month.to_i, -1).day)).each do |day|
+
+  dow=Date.new(year.to_i,month.to_i,day).wday
+  if dow%7==0
+    print "\n"
+  end
+  print day.to_s.rjust(2) + " "  
+
+end 
+
